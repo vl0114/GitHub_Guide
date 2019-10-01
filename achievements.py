33a3c102e270249aa18b0c2cf43c7ff370e7c695
영수 신 <vl0114@naver.com>
2019-10-01 20:50:57 +0900

    add Achievements module, ignore pycache

---
+++ achievements.py
@@ -0,0 +1,7 @@
+# 도전과제를 관리하는 모듈
+
+class Achievement:
+    name = ''
+    require = ''
+
+    pass
---
+++ gdb.py
@@ -0,0 +1,13 @@
+# GitHub 유저의 정보를 관리하는 모듈
+
+class GH_User:
+
+    mail = '' # mail
+    name = '' # GitHub Name
+    gh_id = '' # GitHub ID
+    gh_code = '' # GitHub 고유 코드
+    follower = 0 # 팔로워 수
+    star = 0 # 얻은 별의 수
+    forked = 0 # 포크당한 수
+    repos = 0 # 리포지토리 수
+    commit = 0 # 커밋한 횟수
